83eea1124570998ac5887ad045e8c14f22ae18f8
Omotosho Boluwatife <legendaryboluwatife@gmail.com>
2025-08-16 21:15:05 +0100

    Update login.js

--- themes/custom-software-engineer/login/resources/js/login.js
+++ themes/custom-software-engineer/login/resources/js/login.js
@@ -1,16 +1,15 @@
-// Password visibility toggle functionality
-function togglePasswordVisibility(fieldId) {
-  const passwordField = document.getElementById(fieldId);
-  const eyeOpen = document.getElementById(fieldId + "-eye-open");
-  const eyeClosed = document.getElementById(fieldId + "-eye-closed");
+ function togglePasswordVisibility(fieldId) {
+                var passwordField = document.getElementById(fieldId);
+                var eyeOpen = document.getElementById(fieldId + '-eye-open');
+                var eyeClosed = document.getElementById(fieldId + '-eye-closed');
 
-  if (passwordField.type === "password") {
-    passwordField.type = "text";
-    eyeOpen.classList.add("hidden");
-    eyeClosed.classList.remove("hidden");
-  } else {
-    passwordField.type = "password";
-    eyeOpen.classList.remove("hidden");
-    eyeClosed.classList.add("hidden");
-  }
-}
+                if (passwordField.type === 'password') {
+                    passwordField.type = 'text';
+                    eyeOpen.classList.add('hidden');
+                    eyeClosed.classList.remove('hidden');
+                } else {
+                    passwordField.type = 'password';
+                    eyeOpen.classList.remove('hidden');
+                    eyeClosed.classList.add('hidden');
+                }
+            }
